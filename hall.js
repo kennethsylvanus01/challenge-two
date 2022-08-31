ad99993ffc64730468319fa323352928380cbf6f
Kenneth sylvanus <kennethsylvanus01@gmail.com>
2022-08-31 05:41:52 -0700

    worked on navbar

---
+++ hall.js
@@ -0,0 +1,11 @@
+// console.log("Name: kenneth Sylvanus Ekpa");
+// console.log("ChurchName: Winners");
+// console.log("Age: 28");
+// console.log("Nickname: Crown");
+// console.log(25+1+2);
+
+let ice_urian = "PayDae";
+console.log(typeof(ice_urian));
+
+let kenneth = "Phd";
+console.log(kenneth +" "+ "in a while");
